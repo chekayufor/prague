0d72bbc4fbdbaf05fea9e219e5981ea905180d98
Elena <chekayufor@gmail.com>
2020-08-27 20:36:00 +0300

    after all changes

---
+++ server/client/src/App.js
@@ -0,0 +1,75 @@
+import React, { useEffect, Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
+
+import NavBar from './components/layout/toolBar/NavBar';
+import MainContainer from './components/pages/MainContainer';
+import EuropeTour from './components/pages/EuropeTour';
+import PragueTour from './components/pages/PragueTour';
+import CzechTour from './components/pages/СzechTour';
+import Transfer from './components/pages/Transfer';
+import Tour from './components/tours/Tour';
+import Auth from './components/pages/Auth';
+import Bottom from './components/layout/footer/Bottom';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+import Register from './components/auth/Register';
+import PrivetRoute from './components/routing/PrivetRoute';
+
+import TourState from './context/tour/TourState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
+const App = () => {
+  
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  },[]);
+  
+
+  return (
+    <AuthState>
+    <TourState>
+    <AlertState>
+      <Router>
+        <Fragment>
+          <NavBar/>
+          <Alerts/>
+          <Switch>
+            <Route exact path='/' component = {MainContainer}/>
+            <Route path='/pragueTour' render={(props =>(
+              <PragueTour {...props}/>))}/>
+            <Route path='/czechTour' component = {CzechTour}/>
+            <Route path='/europeTour' component = {EuropeTour}/> 
+            <Route path='/transfer' component = {Transfer}/>
+            <Route path='/register' component = {Register}/>
+            <Route path='/:tours/:tourId' render={(props)=> (
+              <Tour {...props} />)}/>
+              
+              <Route exact path='/login' component={Login} />
+              <PrivetRoute path='/privet' component={Auth}/>
+          </Switch>
+          <Bottom/>
+        </Fragment>
+      </Router>
+      </AlertState>
+    </TourState>
+    </AuthState>
+  );
+}
+
+export default App;
---
+++ server/client/src/components/auth/Register.js
@@ -0,0 +1,118 @@
+import React, { useState, useContext, useEffect } from 'react';
+// import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
+const Register = props => {
+  // const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  // const { setAlert } = alertContext;
+  const { register, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    M.AutoInit();
+      //redirection
+    if (isAuthenticated) {
+      props.history.push('/privet');
+    }
+
+    if (error === 'User already exists') {
+      // setAlert(error, 'danger');
+      M.toast({html:`${error}`})
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = user;
+
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      // setAlert('Please enter all fields', 'danger');
+      M.toast({html:'Пожалуйста заполните все поля'})
+    } else if (password !== password2) {
+      // setAlert('Passwords do not match', 'danger');
+      M.toast({html:'Пароли не эдентичные'})
+    } else {
+      register({
+        name,
+        email,
+        password
+      });
+    }
+  };
+
+  return (
+    <div style={{marginTop:'150px'}} className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input
+            id='name'
+            type='text'
+            name='name'
+            value={name}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChange}
+            required
+            minLength='6'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password2'>Confirm Password</label>
+          <input
+            id='password2'
+            type='password'
+            name='password2'
+            value={password2}
+            onChange={onChange}
+            required
+            minLength='6'
+          />
+        </div>
+        <input
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
